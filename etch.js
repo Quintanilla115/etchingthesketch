bc5e4cf3f1f3c1c5d79b5807a3d0f3349edfcdec
Alec Quintanilla <23916616+Quintanilla115@users.noreply.github.com>
2025-03-22 16:07:30 -0500

    added square in js

---
+++ etch.js
@@ -0,0 +1,10 @@
+
+// this is the box
+const div = document.createElement('div');
+
+div.style.width = '16px';
+div.style.height = '16px';
+div.style.backgroundColor = 'red';
+document.body.appendChild(div);
+
+// box stuff ^^^
