d06b9c0cfa751cc23fb7cd6d62a2c87533f9e6d8
Alec Quintanilla <23916616+Quintanilla115@users.noreply.github.com>
2025-03-23 13:47:31 -0500

    starting to unscrew myself

--- etch.js
+++ etch.js
@@ -4,6 +4,6 @@ grid.computedStyleMap.gridTemplateRows = 'repeat(16, 1fr)';
 
 for (let i = 0; i < 256; i++) {
   let square  = document.createElement('div');
-  square.style.backgroundColor = 'white';
+  square.style.backgroundColor = 'red';
   grid.insertAdjacentElement('beforeend', square);
 }
