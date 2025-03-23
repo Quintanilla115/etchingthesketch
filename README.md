5ca391e317e4c3e0f6f2153083a291993fdeed52
Alec Quintanilla <23916616+Quintanilla115@users.noreply.github.com>
2025-03-22 20:13:54 -0500

    added some flex box for the menu and gid. unfinished

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # etchingthesketch
 
 purpose of this is to create an etchaskethy
+
+step 1 complete
+
--- etch.js
+++ etch.js
@@ -1,10 +1,21 @@
-
 // this is the box
-const div = document.createElement('div');
+//const div = document.createElement('div');
 
-div.style.width = '16px';
-div.style.height = '16px';
-div.style.backgroundColor = 'red';
-document.body.appendChild(div);
+//div.style.width = '16px';
+//div.style.height = '16px';
+//div.style.backgroundColor = 'red';
+//document.body.appendChild(div);
+
 
 // box stuff ^^^
+// creating the grid
+
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            
+           
+        }
+    }
+}
+
