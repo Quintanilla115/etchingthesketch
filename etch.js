f212cfc5aabe5a562b2d467260ced0c3a19d7db1
Alec Quintanilla <23916616+Quintanilla115@users.noreply.github.com>
2025-03-23 18:29:28 -0500

    Able to adjust grid size through select button from defualt

--- etch.js
+++ etch.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
+    createBoard(16)
 
-    createBoard(32)
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function() {
+        let size = getSize();
+        createBoard(size);
+    });
 
 
 });
 
 function createBoard(size){
-    let board = document.querySelector('.board'):
+    
+    let board = document.querySelector('.board')
     
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -18,6 +24,19 @@ function createBoard(size){
         div.style.backgroundColor = 'red';
         board.insertAdjacentElement('beforeend', div);
     }
+}
 
+function getSize(){
+    let input = prompt('Enter the size of the board');
+    if (input == ""){
+        message.innerHTML = ('Please enter a number');
+    }
+
+    else if ( input <2 || input > 100){
+        alert('Please enter a number between 2 and 100');
+        message.innerHTML = ('Enter a number between 2 and 100');
+    }
+ 
+    return input;
 
 }
